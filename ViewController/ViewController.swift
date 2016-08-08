6aa9b34f0c2d48f3f8a453ceeee00902e3cc58f0
Yesi <317447721@qq.com>
2016-08-08 14:17:09 +0800

    Initial Commit

---
+++ ViewController/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ViewController
+//
+//  Created by Yesi on 16/8/8.
+//  Copyright © 2016年 Yesi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
